d9640256f8dea0357d833ae15ce93dfe4105709f
arun <crcravi66@gmail.com>
2024-02-06 16:10:37 +0530

    start the project

--- practice.css/animation.js
+++ practice.css/animation.js
@@ -1,8 +1,1 @@
-const onoffbox = document.querySelector(".onoffbox");
-const centercircle = document.querySelector(".centercircle");
 
-
-
-onoffbox= addEventListener("click", () =>{
-    centercircle.classList.toggle("onbtn")
-})
