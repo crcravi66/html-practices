00122be9fb86e6539947cf37bda1c030cf57a4cb
arun <crcravi66@gmail.com>
2024-03-09 15:27:55 +0530

    practices for addeventlistern

--- practice.css/indexapp.js
+++ practice.css/indexapp.js
@@ -9,3 +9,17 @@ v2Btn.onclick = function() {
 document.querySelector('h2').onclick = function(){
     console.log("you clicked for h2")
 }
+
+            // Add event btn 
+const eventBtn = document.querySelector(".event");
+
+eventBtn.addEventListener('click',clickFunc);
+eventBtn.addEventListener("mouseover",hoverHere);
+
+function clickFunc(){
+    document.querySelector("#addevent").innerHTML += "you clicked <br>";
+}
+
+function hoverHere(){
+    document.querySelector("#addevent").innerHTML += "hover here <br>";
+}   
