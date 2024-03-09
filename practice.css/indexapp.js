91c407c1fef155a0b8884e1b55ea2eea97b300ff
arun <crcravi66@gmail.com>
2024-03-09 12:30:41 +0530

    practises for javascript

---
+++ practice.css/indexapp.js
@@ -0,0 +1,11 @@
+const v2Btn = document.querySelector('#verson2')
+
+v2Btn.onclick = function() {
+    console.log('hi there you clicked me ')
+    console.log('what you want')
+
+}
+
+document.querySelector('h2').onclick = function(){
+    console.log("you clicked for h2")
+}
